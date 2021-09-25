262f43bc658b4b2180dda02d1d855a0989d968b3
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-09-25 03:04:44 -0300

    estilizando a tela

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
+const firstPokemon = document.querySelector('.first-pokemon');
+const secondPokemon = document.querySelector('.second-pokemon')
+const thirdPokemon = document.querySelector('.third-pokemon');
+const fourthPokemon = document.querySelector('.fourth-pokemon');
 
-const getPokemon = document.querySelector('.first-pokemon');
-const arrayPokemon = [firstPokemon, secondPokemon, thirdPokemon, fourPokemon]
 
 const URL_Fetch_Api = async (pokemon) => { 
   return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
   .then((resposta) => resposta.json())
+};
+
+const generateRandomNumber = () => Math.floor(Math.random() * 201);
+
+async function generateRandomPokemons(pokemon) {
+  const api = await URL_Fetch_Api(generateRandomNumber());
+  firstPokemon.src = api.sprites.front_default;
+  const api2 = await URL_Fetch_Api(generateRandomNumber());
+  secondPokemon.src = api2.sprites.front_default;
+  const api3 = await URL_Fetch_Api(generateRandomNumber());
+  thirdPokemon.src = api3.sprites.front_default;
+  const api4 = await URL_Fetch_Api(generateRandomNumber());
+  fourthPokemon.src = api4.sprites.front_default;
 }
 
-async function teste() {
-  const api = await URL_Fetch_Api('blastoise');
-  getPokemon.src = api.sprites.front_default;
-  console.log(api);
-  console.log(getPokemon)
-}
-
-teste();
+generateRandomPokemons();
